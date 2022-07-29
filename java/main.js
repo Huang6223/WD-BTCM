9cd6c1d46f6db6cf6ebb25a2d681e11f45400909
Huang6223 <108096884+Huang6223@users.noreply.github.com>
2022-07-29 17:11:43 +0700

    Add files via upload

---
+++ java/main.js
@@ -0,0 +1,54 @@
+var typed = new Typed('.element', {
+  strings: ['start','HUANG MING HUI','HWANG MIN HEE'],
+    typeSpeed: 100,
+	backSpeed: 50,
+	smartBackspace: true,
+ loop: true,
+  loopCount: Infinity,
+	showCursor: true,
+  cursorChar: '|',
+  autoInsertCss: true,
+backDelay: 2000,
+});
+
+var typed = new Typed('.element-2', {
+  strings: ['start','An Editor','An Illustrator'],
+    typeSpeed: 100,
+	backSpeed: 50,
+	smartBackspace: true,
+ loop: true,
+  loopCount: Infinity,
+	showCursor: true,
+  cursorChar: '|',
+  autoInsertCss: true,
+backDelay: 2000,
+});
+
+var typed = new Typed('.element-3', {
+  strings: ['start','00','00','00','01','01','01','02','02','03','02','01','01'],
+    typeSpeed: 0,
+	backSpeed: 0,
+	showCursor: false,
+  autoInsertCss: true,
+});
+var typed = new Typed('.element-4', {
+  strings: ['start','00','00','00','01','03','03','05','05','05','05','07','07','07','09','09','10'],
+    typeSpeed: 0,
+	backSpeed: 0,
+	showCursor: false,
+  autoInsertCss: true,
+});
+var typed = new Typed('.element-5', {
+  strings: ['start','0.0','0.0','0.0','0.1','0.1','0.2','0.4','0.6','0.7','0.7','0.8',],
+    typeSpeed: 0,
+	backSpeed: 0,
+	showCursor: false,
+  autoInsertCss: true,
+});
+var typed = new Typed('.element-6', {
+  strings: ['start','00','00','00','01','01','02','02'],
+    typeSpeed: 0,
+	backSpeed: 0,
+	showCursor: false,
+  autoInsertCss: true,
+});
